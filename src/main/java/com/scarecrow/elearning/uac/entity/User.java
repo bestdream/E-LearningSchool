473c939dfdec9b20600325620465f506ce4594e1
bestdream <1113470015@qq.com>
2018-03-11 21:49:06 +0800

    mybatis集成测试

--- src/main/java/com/scarecrow/elearning/uac/entity/User.java
+++ src/main/java/com/scarecrow/elearning/uac/entity/User.java
@@ -1,5 +1,40 @@
 package com.scarecrow.elearning.uac.entity;
 
 public class User {
-
+	
+	public int id;
+	public String name;
+	public String sex;
+	public int age;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", sex=" + sex + ", age=" + age + "]";
+	}
+	
+	
 }
